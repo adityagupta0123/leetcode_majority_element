1d19dd1037fe98d1190137e84717ca04a5c8ca33
ADITYA GUPTA <78367281+adityagupta0123@users.noreply.github.com>
2022-03-23 21:21:44 +0530

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -13,4 +13,32 @@ class Solution {
         }
         return candinate;
     }
+    
+}
+___________________OR________________
+
+class Solution {
+     public int majorityElement(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int count;
+        for (int num : nums) {
+            if (map.containsKey(num)) {
+                count = map.get(num);
+                map.put(num, ++count);
+            } else {
+                map.put(num, 1);
+            }
+        }
+
+        int max = 0;
+        int output = 0;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > max) {
+                max = entry.getValue();
+                output = entry.getKey();
+            }
+        }
+
+        return output;
+    }
 }
